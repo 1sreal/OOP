11a158cf0be2daaea2d5c07cd3a05058ef8d81a1
1sreal <niibenedict123@gmail.com>
2023-03-23 20:47:05 +0000

    tells ones current age

---
+++ Labs/Readme.md
@@ -0,0 +1,2 @@
+A short program that asks the user for their age and responds with the year the
+user was born in ls
---
+++ Labs/age.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int c_year, c_age, b_year;
+    c_year = 2023;
+    cout << "Enter your age: "<< endl;
+    cin >> c_age;
+    b_year = c_year - c_age;
+    cout<< "Your year of birth is "<< b_year<< endl;
+    cout<< "Thank You!!" << endl;
+    return 0;
+    }
